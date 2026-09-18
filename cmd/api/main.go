412e7b6e476fb16618251153bedec66240cb7a8a
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-09-18 10:18:04 +0530

    version upgrade + main file more code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "log"
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
 type app struct {
 	config config
@@ -18,5 +27,58 @@ func main() {
 }
 
 func run() error {
+
+	var cfg config
+
+	// take in info from terminal at server start time
+	flag.StringVar(&cfg.addr, "addr", envOrVar("ZEKE_ADDR", ":6969"), "API server address")
+	flag.StringVar(&cfg.env, "env", envOrVar("ZEKE_ENV", "dev"), "environment mode for address")
+	flag.Parse()
+
+	// handle SIGINT gracefully (CTRL + C)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// handle shutdown properly, ensure no leaks, close everything here
+	defer func() {
+		// pass for now
+	}()
+
+	// create the app and create the net listener
+	app := &app{
+		config: cfg,
+	}
+
+	srv := &http.Server{
+		Addr:         cfg.addr,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Minute * 2,
+		IdleTimeout:  time.Minute * 1,
+		BaseContext:  func(net.Listener) context.Context { return ctx },
+		Handler:      app.routes(),
+	}
+
+	// handle interrupts
+	srvErr := make(chan error, 1)
+	go func(){
+		srvErr <- srv.ListenAndServe()
+	}()
+
 	return nil
+}
+
+func(app *app) routes() http.Handler {
+	mux:= http.NewServeMux()
+
+	mux.HandleFunc("/upload", nil)
+
+	return mux
+}
+
+func envOrVar(key, defaultVal string) string {
+	value := os.Getenv(key)
+	if value != "" {
+		return value
+	}
+	return defaultVal
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/DivyanshuShekhar55/zeke 
 
-go 1.23.3
+go 1.27.1
